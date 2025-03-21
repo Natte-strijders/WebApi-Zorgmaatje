47f4b65211bbcf6088e528f924f40c3ff88517d1
KyanPat <kyantsoi1@gmail.com>
2025-03-21 22:13:56 +0100

    begin aan arts api

---
+++ WereldbouwerAPI/Arts/Arts.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZorgmaatjeWebApi.Arts
+{
+    public class Arts
+    {
+        public string id { get; set; }
+        [Required]
+        public string naam { get; set; }
+        public string specialisatie { get; set; }
+    }
+}
