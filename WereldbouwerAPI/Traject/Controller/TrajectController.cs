c84d8076e4d8e542caf6c8682b8fd995cb590c36
KyanPat <kyantsoi1@gmail.com>
2025-03-21 21:01:07 +0100

    traject repository toegevoegd

---
+++ WereldbouwerAPI/Traject/Controller/TrajectController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using ZorgmaatjeWebApi.Patient.Repositories;
+
+namespace ZorgmaatjeWebApi.Traject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TrajectController : ControllerBase
+    {
+        private readonly IPatientRepository _patientRepository;
+        private readonly IAuthenticationService _authenticationService;
+        private readonly ILogger<TrajectController> _logger;
+
+        public TrajectController(IPatientRepository repository, IAuthenticationService authenticationService, ILogger<TrajectController> logger)
+        {
+            _patientRepository = repository;
+            _authenticationService = authenticationService;
+            _logger = logger;
+        }
+
+        [HttpGet("GetUserId", Name = "GetUserId")]
+        [Authorize]
+        public ActionResult<string> GetUserId()
+        {
+            var userId = _authenticationService.GetCurrentAuthenticatedUserId();
+            return Ok(userId);
+        }
+
+
+    }
+}
---
+++ WereldbouwerAPI/Traject/Repositories/TrajectRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ZorgmaatjeWebApi.Traject.Repositories
+{
+    public class TrajectRepository : ITrajectRepository
+    {
+        private readonly string sqlConnectionString;
+
+        public TrajectRepository(string sqlConnectionString)
+        {
+            this.sqlConnectionString = sqlConnectionString;
+        }
+
+       
+    }
+}
